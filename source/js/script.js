d4c72724263119cdf6b0fbc91a55ce3058493de1
Kettel <mr.korovin87@mail.ru>
2021-06-03 17:07:21 +0500

    Закончил catalog.html

--- source/js/script.js
+++ source/js/script.js
@@ -2,16 +2,19 @@ let navigation = document.querySelector('.filter__select');
 let hideButton = document.querySelector('.filter__top-button');
 let filterButton = document.querySelector('.filter__button')
 let clientsLike = document.querySelectorAll('.clients__likes-box');
+let filterLink = document.querySelectorAll('.filter__link')
+let filterLinkActive = document.querySelector('.filter__link--active')
 
 hideButton.addEventListener('click', function (evt) {
   evt.preventDefault();
   navigation.classList.toggle('filter__select--show')
 
   if(navigation.classList.contains('filter__select--show')) {
-    hideButton.innerHTML = 'Свернуть'
+
+    hideButton.innerHTML = '<img src="img/icon_close.svg" class="filter-top-button-icon--close">'
     hideButton.classList.add('filter__top-button-js')
   } else {
-    hideButton.innerHTML = 'Показать все';
+    hideButton.innerHTML = '<img src="img/3-dots.svg" class="filter-top-button-icon">';
     hideButton.classList.remove('filter__top-button-js')
   }
 });
@@ -19,6 +22,8 @@ hideButton.addEventListener('click', function (evt) {
 filterButton.addEventListener('click', function (evt) {
   evt.preventDefault();
   navigation.classList.remove('filter__select--show');
+  hideButton.classList.remove('filter__top-button-js')
+  hideButton.innerHTML = '<img src="img/3-dots.svg" class="filter-top-button-icon">';
 });
 
 // Цикл проверяет нажимает ли пользователь лайк
@@ -27,4 +32,13 @@ for (let i = 0; i < clientsLike.length; i ++) {
     evt.preventDefault()
     clientsLike[i].classList.toggle('clients__likes-box--active')
   })
+};
+
+//Фильтрация по странам
+function tabs (evt) {
+  evt.preventDefault()
+  for (let i = 0; i < filterLink.length; i++) {
+    filterLink[i].className = filterLink[i].className.replace(' filter__link--active', '')
+  }
+  evt.currentTarget.className += " filter__link--active";
 }
